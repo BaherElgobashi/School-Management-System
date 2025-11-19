f1cc29fccb32576e43d1045dfc07f6ad8102d8a1
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2025-11-19 14:19:01 +0200

    Add Student Model

---
+++ School Management System Solution/School Management System/Models/StudentClass.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace School_Management_System.Models
+{
+    public class StudentClass
+    {
+        //- Id, StudentId, ClassId, EnrollmentDate
+        //- Represents enrollment of a student in a class 
+
+        public int Id { get; set; }
+        // Foregin Key For User Table.
+        public int StudentId { get; set; }
+        // Navigation Property for User Table.
+        public User User { get; set; }
+
+        // Foregin Key for Class Table.
+        public int ClassId { get; set; }
+        // Navigation Property for Class Table.
+        public Class Class { get; set; }
+
+    }
+}
